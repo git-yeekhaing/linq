ab5e42fa34ec1ea487ee860ea2affd897aad773d
git-yeekhaing <yee.khaing.git@gmail.com>
2020-06-26 14:26:00 +0900

    linq syntax

--- LinqBasics/LinqBasics/Program.cs
+++ LinqBasics/LinqBasics/Program.cs
@@ -6,10 +6,74 @@ using System.Threading.Tasks;
 
 namespace LinqBasics
 {
+    // Example: We have an integer list and we need to write a LINQ query which will return all the integers which are greater than 5.
+    // We are going to create a console application.
     class Program
     {
         static void Main(string[] args)
         {
+            LinqQuerySyntax();
+
+            Console.WriteLine("\n==============================");
+            LinqMethodSyntax();
+            Console.WriteLine("\n==============================");
+            LinqMixedSyntax();
+            Console.ReadKey();
+        }
+
+        private static void LinqQuerySyntax()
+        {
+            // data source
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            // linq query using query syntax
+            var QuerySyntax = from obj in integerList
+                              where obj > 5
+                              select obj;
+
+            // Execution
+            foreach (var item in QuerySyntax)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    
+        private static void LinqMethodSyntax()
+        {
+            //Data Source
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            //LINQ Query using Method Syntax
+            var methodSyntax = integerList.Where(obj => obj > 5).ToList();
+
+            // Execution
+            foreach(var item in methodSyntax)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        private static void LinqMixedSyntax()
+        {
+            //Data Source
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            // mixed syntax
+            var mixsyntax = (from obj in integerList
+                             where obj > 5
+                             select obj).Sum();
+
+            //Execution
+            Console.Write("Sum Is : " + mixsyntax);
         }
     }
 }
