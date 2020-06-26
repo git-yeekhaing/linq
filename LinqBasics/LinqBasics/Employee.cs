c6e0bd6e0acf98d8e74b7d0188dc0de3a4eb3c63
git-yeekhaing <yee.khaing.git@gmail.com>
2020-06-26 18:40:36 +0900

    select

---
+++ LinqBasics/LinqBasics/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqBasics
+{
+   public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+    }
+}
